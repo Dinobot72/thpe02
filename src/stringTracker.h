e830c1537d90fa05181d95fe1af90a5652dad034
Dylan Regan <dyregan6706@gmail.com>
2025-11-06 14:31:28 -0700

    assigning and creating functions

--- src/stringTracker.h
+++ src/stringTracker.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+// #include <catch2/catch_test_macros.hpp> 
 using namespace std;
 
 class StringTracker
@@ -11,10 +12,23 @@ class StringTracker
         StringTracker();
         ~StringTracker();
 
-        bool addString(const string& str);
-        void print(ostream& out) const;
+        // Teammate 1's functions
+        bool addString( const string& str );
+        void print( ostream& out ) const;
+        bool removeString( const string& str );
+        bool findString( const string str ) const;
+        bool incrementCount( const string str );
+        bool decrementCount( const string str );
+        bool empty() const;
 
-        // other functions
+
+        // Teammate 2's functions
+        int getCount( const string str ) const;
+        int getMaximumcount() const;
+        int size() const;
+        void clear();
+        void printPhrases( ostream& out ) const;
+        void printCounters( ostream& out) const;
 
     private:
         struct Node
--- src/stringTracker1.cpp
+++ src/stringTracker1.cpp
@@ -1,2 +1,65 @@
 #include "stringTracker.h"
 
+StringTracker::StringTracker()
+{
+    Node *head = nullptr;
+}
+
+StringTracker::~StringTracker()
+{
+    clear();
+}
+
+bool StringTracker::empty() const
+{
+    return head == nullptr;
+}
+
+
+bool StringTracker::addString( const string& str )
+{
+    return true;
+}
+
+void StringTracker::print( ostream& out ) const
+{
+    Node *temp;
+    temp = this->head;
+    while( temp != nullptr )
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+}
+
+bool StringTracker::removeString( const string& str )
+{
+    return true;
+
+}
+
+bool StringTracker::findString( const string str ) const
+{
+    Node *temp;
+    temp = this->head;
+    while( temp != nullptr )
+    {
+        if( temp->value == str )
+        return true;
+
+        temp = temp->next;
+    }
+
+}
+
+bool StringTracker::incrementCount( const string str )
+{
+    return true;
+
+}
+
+bool StringTracker::decrementCount( const string str )
+{
+    return true;
+
+}
