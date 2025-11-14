a5f4e659b27e1d1052a46972c5e8ca2de7c09187
Dylan Regan <dyregan6706@gmail.com>
2025-11-14 12:04:57 -0700

    updated instructor tests

--- src/insttests.cpp
+++ src/insttests.cpp
@@ -1,8 +1,7 @@
 #include <sstream>
 #include "stringTracker.h"
-#include <catch2/catch_test_macros.hpp> 
 
-TEST_CASE("StringTracker::addString - add to empty list")
+TEST_CASE("StringTracker::addString - RS add to empty list")
 {
     bool retValue;
     string addValue = "The Banana";
@@ -16,5 +15,26 @@ TEST_CASE("StringTracker::addString - add to empty list")
     
     myTracker.print( sout );
     REQUIRE( sout.str() == "The Banana (1)" );
+    
+}
+
+TEST_CASE("StringTracker::addString - RS try duplicates being added to list")
+{
+    bool retValue;
+    string addValue = "Bananas";
+    ostringstream sout;
+
+    StringTracker myTracker;
+
+    
+    retValue = myTracker.addString( addValue );
+    REQUIRE( retValue );
+
+    retValue = myTracker.addString( addValue );
+    REQUIRE( retValue );
+    
+    // check printout with output string stream
+    myTracker.print( sout );
+    REQUIRE( sout.str() == "Bananas (1)" );
     
 }
